ca8cb63e87c0ab1051038dd3300965d47754a533
LapsTimeOFF <quentinloulou31@gmail.com>
2020-09-20 11:46:03 +0200

    update

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 require('dotenv').config();
+const Discord = require('discord.js');
+const fs = require('fs');
+
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} est en ligne`);
+})
+
+client.login();
